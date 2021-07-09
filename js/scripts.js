dc50cd52d0c2fc292d9d10fb20f3655b71f1375d
hoshi-R <hs.rt7771@gmail.com>
2021-07-09 23:26:42 +0900

    更新-7.9

--- js/scripts.js
+++ js/scripts.js
@@ -7,55 +7,9 @@
 // Scripts
 // 
 
-// window.addEventListener('DOMContentLoaded', event => {
-
-//     // Navbar shrink function
-//     var navbarShrink = function () {
-//         const navbarCollapsible = document.body.querySelector('#mainNav');
-//         if (!navbarCollapsible) {
-//             return;
-//         }
-//         if (window.scrollY === 0) {
-//             navbarCollapsible.classList.remove('navbar-shrink')
-//         } else {
-//             navbarCollapsible.classList.add('navbar-shrink')
-//         }
-
-//     };
-
-//     // Shrink the navbar 
-//     navbarShrink();
-
-//     // Shrink the navbar when page is scrolled
-//     document.addEventListener('scroll', navbarShrink);
-
-//     // Activate Bootstrap scrollspy on the main nav element
-//     const mainNav = document.body.querySelector('#mainNav');
-//     if (mainNav) {
-//         new bootstrap.ScrollSpy(document.body, {
-//             target: '#mainNav',
-//             offset: 74,
-//         });
-//     };
-
-//     // Collapse responsive navbar when toggler is visible
-//     const navbarToggler = document.body.querySelector('.navbar-toggler');
-//     const responsiveNavItems = [].slice.call(
-//         document.querySelectorAll('#navbarResponsive .nav-link')
-//     );
-//     responsiveNavItems.map(function (responsiveNavItem) {
-//         responsiveNavItem.addEventListener('click', () => {
-//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
-//                 navbarToggler.click();
-//             }
-//         });
-//     });
-
-// });
-
 // アニメーション
 const targetElement = document.querySelectorAll(".animationTarget");
-document.addEventListener("scroll", function() {
+document.addEventListener("scroll", () => {
 
     for (let i = 0; i < targetElement.length; i++) {
         const getElementDistance = targetElement[i].
@@ -79,7 +33,7 @@ document.addEventListener("scroll", function() {
 function loaded() {
     document.getElementById("loading").classList.remove("active")
 }
-window.addEventListener("load", function() {
+window.addEventListener("load", () => {
     this.setTimeout(loaded, 1200)
 });
 setTimeout(loaded, 5000);
@@ -87,12 +41,12 @@ setTimeout(loaded, 5000);
 // ナビゲーション
 
 const responsiveNav = document.getElementById("menuButton");
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     
     responsiveNav.addEventListener("click", function() {
         this.classList.toggle("on")
     });
-    responsiveNav.addEventListener("click", function() {
+    responsiveNav.addEventListener("click", () => {
         document.getElementById("navbarResponsive").classList.toggle("on")
     });
 });
@@ -134,8 +88,8 @@ else if(hour >= 18 && hour <= 23){
 // jQuery試運転
 
 $(function() {
-    $('#test').click(function() {
-        $('#kieru').slideUp();
+    $('#test-toggle').click(function() {
+        $('#test-toggleTarget').slideToggle();
     });
 
 });
